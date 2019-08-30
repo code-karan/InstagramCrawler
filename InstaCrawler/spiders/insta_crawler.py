e17251798e624bb7fb6585754ee63ed95ca05b71
“Karan <ksinghasane14@gmail.com>
2019-08-30 14:21:49 +0530

    Working for all posts

--- InstaCrawler/spiders/insta_crawler.py
+++ InstaCrawler/spiders/insta_crawler.py
@@ -5,42 +5,50 @@ from scrapy.selector import Selector
 from scrapy.http import Request
 from time import sleep
 
+# paste username of profile to be scraped
+# ---------Here 👇️👇️--------------------
+user_id = 'thisisbillgates'
+
+#---------------------------------------------------
+
 
 class InstaCrawlerSpider(Spider):
     name = 'insta_crawler'
-    allowed_domains = ['instagram.com/chrishemsworth/']
+    global user_id
+    allowed_domains = ['instagram.com/{}/'.format(user_id)]
 
     def start_requests(self):
+        global user_id
         #opt = webdriver.ChromeOptions()
         #opt.add_extension("/home/karan/Desktop/InstaCrawler/Block-image_v1.1.crx")
         #browser = webdriver.Chrome(chrome_options=opt)
         self.driver = webdriver.Chrome('/home/karan/Desktop/chromedriver')
 
         # get profile page
-        self.driver.get('https://www.instagram.com/chrishemsworth')
+        self.driver.get('https://www.instagram.com/{}'.format(user_id))
         
         
         
-        
-
         #scroll through an infinite page
 
         # Get scroll height
         last_height = self.driver.execute_script("return document.body.scrollHeight")
+        scroll_list = []
+        self.post_caption_list = []
 
         while True:
                 # Scroll down to bottom
                 self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
                 # Wait to load page
-                sleep(6)
+                sleep(3)
                 # assign page source
                 self.select_profile = Selector(text = self.driver.page_source)
 
                 # fetch all posts
                 sleep(1)
                 posts = self.select_profile.xpath('//*[@class="v1Nh3 kIKUG  _bz0w"]/a/@href').extract()
-                self.post_caption_list = []
+                
                 #self.post_likes_list = []
                 for post in posts:
                     sleep(1)
@@ -55,14 +63,30 @@ class InstaCrawlerSpider(Spider):
 
                     #self.post_likes = self.driver.find_element_by_xpath('//*[@class="_0mzm- sqdOP yWX7d    _8A5w5    "]/span').text
                     #self.post_likes_list.append(self.post_likes)
+
+                    yield Request(post_url, callback = self.parse_insta)
+
+
                 #done with posts
+                scroll_list.append(last_height)
 
                 # back to profile page
-                self.driver.get('https://www.instagram.com/chrishemsworth')
+                self.driver.get('https://www.instagram.com/{}'.format(user_id))
+
 
                 # scroll to last height
-                self.driver.execute_script("window.scrollTo(0, {});".format(last_height))
-                sleep(8)
+                for scroll in scroll_list:
+                    self.driver.execute_script("window.scrollTo(0, {});".format(scroll))
+                    sleep(4)
+                    self.driver.execute_script("window.scrollTo(0, {});".format(scroll+1095))
+                    sleep(3)
+                    self.driver.execute_script("window.scrollTo(0, {});".format(scroll+2190))
+                    sleep(3)
+                    self.driver.execute_script("window.scrollTo(0, {});".format(scroll+3285))
+                    sleep(3)
+                    self.driver.execute_script("window.scrollTo(0, {});".format(scroll+4380))
+                    sleep(3)
+                    self.driver.execute_script("window.scrollTo(0, {});".format(scroll+5475))
 
 
                 # Calculate new scroll height and compare with last scroll height
@@ -71,19 +95,6 @@ class InstaCrawlerSpider(Spider):
                     break
                 last_height = new_height
         
-
-
-
-        
-        
-        
-        
-
-
-
-                
-        
-        yield Request(post_url, callback = self.parse_insta)
     
     
     def parse_insta(self, response):
